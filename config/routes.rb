eb80e4288ede07ce7eaa10ffbdc235ae9f802e5d
daikideal <54879050+daikideal@users.noreply.github.com>
2019-11-13 22:28:34 +0900

    Add: Job offererのdeviseルーティングを追加
    生成されていたデフォルトのルーティングから変更、追加した。
    これによりユーザー種別ごとにモデル・コントローラ・ビューが管理できるようになるらしい。
    参考(https://qiita.com/Yama-to/items/54ab4ce08e126ef7dade)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :job_offerers
+  devise_for :job_offerers, controllers: {
+    sessions: 'job_offerers/sessions',
+    passwords: 'job_offerers/passwords',
+    registrations: 'job_offerers/registrations'
+  }
   root 'home#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
