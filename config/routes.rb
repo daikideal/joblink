a6fb26d302e8449d9957f80aa6f65eda909a2aee
daikideal <54879050+daikideal@users.noreply.github.com>
2020-01-13 14:08:53 +0900

    Add: JobSeekerがメールアドレス確認が必要な操作をした時遷移する画面を追加
    そのままではメールアドレス認証を経ないままログイン画面に遷移してしまうためメールアドレスを確認するように促す画面が必要だと考えた。

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,10 @@ Rails.application.routes.draw do
     confirmations: 'job_seekers/confirmations'
   }
 
+  devise_scope :job_seeker do
+    get 'confirm_email', to: 'job_seekers/registrations#confirm_email'
+  end
+
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
