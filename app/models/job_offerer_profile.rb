6de671cf874fd7cccbcf5a5af20cf2ecc038d478
daikideal <54879050+daikideal@users.noreply.github.com>
2020-01-16 01:07:54 +0900

    Add: JobOfferersProfileとJobSeekerProfileにフルネーム出力メソッドを追加
    フルネームを簡潔に表示させたいと思ったため。

--- app/models/job_offerer_profile.rb
+++ app/models/job_offerer_profile.rb
@@ -5,6 +5,14 @@ class JobOffererProfile < ApplicationRecord
             :first_name_furigana, :last_name_furigana,
             presence: true
 
+  def full_name
+    last_name + ' ' + first_name
+  end
+
+  def full_name_furigana
+    last_name_furigana + ' ' + first_name_furigana
+  end
+
   def shaped_avatar
     avatar.variant(resize_to_fill: [200, 200]).processed
   end
--- app/models/job_seeker_profile.rb
+++ app/models/job_seeker_profile.rb
@@ -6,6 +6,14 @@ class JobSeekerProfile < ApplicationRecord
             :first_name_furigana, :last_name_furigana,
             presence: true
 
+  def full_name
+    last_name + ' ' + first_name
+  end
+
+  def full_name_furigana
+    last_name_furigana + ' ' + first_name_furigana
+  end
+
   def shaped_avatar
     avatar.variant(resize_to_fill: [200, 200]).processed
   end
