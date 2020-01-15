f3937f461c225f7ae2050f24e0be2a5ac97c988e
daikideal <54879050+daikideal@users.noreply.github.com>
2020-01-15 23:35:27 +0900

    Change: JobOffererProfileのavatar表示方法を変更
    viewのコードを短くできるのでJobSeekerProfileと同様にmodelのメソッドで画像をリサイズするやり方にした。
    それに伴い、showのimage_tagもJobSeekerProfileと同じ記法とした。

--- app/models/job_offerer_profile.rb
+++ app/models/job_offerer_profile.rb
@@ -4,4 +4,8 @@ class JobOffererProfile < ApplicationRecord
   validates :first_name, :last_name,
             :first_name_furigana, :last_name_furigana,
             presence: true
+
+  def shaped_avatar
+    avatar.variant(resize_to_fill: [200, 200]).processed
+  end
 end
