f1c7ef69a1d28071edeb91559c9f4bf0f8b3711a
daikideal <54879050+daikideal@users.noreply.github.com>
2020-01-18 13:37:27 +0900

    Add: Messageモデルを追加
    実際にメッセージの内容をtext型でもつMessageのmodelを生成し、マイグレートした。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_13_062406) do
+ActiveRecord::Schema.define(version: 2020_01_18_042633) do
 
   create_table "action_text_rich_texts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -127,6 +127,12 @@ ActiveRecord::Schema.define(version: 2020_01_13_062406) do
     t.index ["email"], name: "index_job_seekers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_job_seekers_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_job_seekers_on_unlock_token", unique: true
+  end
+
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
