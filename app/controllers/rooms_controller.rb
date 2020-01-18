6590c181eb51f8f89b8f7c1d61d38bc01f5f15f9
daikideal <54879050+daikideal@users.noreply.github.com>
2020-01-18 15:22:21 +0900

    Change: いずれかのユーザーでログインを要求するメソッドを追加
    before_actionにauthenticate_job_offerer!とauthenticate_job_seeker!の両方を指定すると、片方でログインしたあともう片方でさらにログインを要求されてしまったので、「どちらかでログインしている場合は何もしない」メソッドを新しく定義し、before_actionに指定した。
    ただ、今のままでは強制的にJobOffererのログインページに行くので、どちらのログインページにリダイレクトさせるかを分岐させる仕組みを考えたい。

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,8 +1,17 @@
 class RoomsController < ApplicationController
-  before_action :authenticate_job_offerer!
-  before_action :authenticate_job_seeker!
+  before_action :authenticate_user!
   
   def show
     @messages = Message.all
+  end
+
+  protected
+
+  def authenticate_user!
+    return if job_offerer_signed_in? || job_seeker_signed_in?
+
+    return authenticate_job_offerer! unless job_offerer_signed_in?
+
+    return authenticate_job_seeker! unless job_seeker_signed_in?
   end
 end
