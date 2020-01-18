c756f903dc1f0ce30ce2a367085f473e1bb2d11a
daikideal <54879050+daikideal@users.noreply.github.com>
2020-01-18 13:59:08 +0900

    Add: roomsコントローラの中身を追加
    チャットは登録ユーザーの機能であるべきなので、job_offererとjob_seekerがそれぞれログインしていなければroomにアクセスできないようにした。
    showアクションはmessageの一覧を取得する。

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,8 @@
 class RoomsController < ApplicationController
+  before_action :authenticate_job_offerer!
+  before_action :authenticate_job_seeker!
+  
   def show
+    @messages = Message.all
   end
 end
