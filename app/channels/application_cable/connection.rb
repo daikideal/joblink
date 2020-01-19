1b849e62f3f5458c9ec5e0c6bba39312804208a5
daikideal <54879050+daikideal@users.noreply.github.com>
2020-01-20 02:37:42 +0900

    Add: ログインユーザーの種別を判別してconnectする条件分岐を追加
    'class RoomChannel'内ではsessionが使用できないのでdeviseの'current_***'がNoMethodになる。
    そこで、'env["warden"].user("scope_type")'でsessionにアクセスし、そのscopeの種類に応じてcurrentをセットすることで現在ログイン中の'current_***'の情報を取得できるようにした。
    参考(https://qiita.com/yusaku_/items/4fdb95aeff2bde1d21ae#appchannelsapplication_cableconnectionrb)

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,30 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_job_offerer
+    identified_by :current_job_seeker
+
+    def connect
+      if env['warden'].user('job_offerer')
+        self.current_job_offerer = find_verified_job_offerer
+      elsif env['warden'].user('job_seeker')
+        self.current_job_seeker = find_verified_job_seeker
+      end
+    end
+
+    private
+
+    def find_verified_job_offerer
+      verified_job_offerer = JobOfferer.find_by(id: env['warden'].user('job_offerer').id)
+      return reject_unauthorized_connection unless verified_job_offerer
+
+      verified_job_offerer
+    end
+
+    def find_verified_job_seeker
+      verified_job_seeker = JobSeeker.find_by(id: env['warden'].user('job_seeker').id)
+      return reject_unauthorized_connection unless verified_job_seeker
+
+      verified_job_seeker
+    end
   end
 end
