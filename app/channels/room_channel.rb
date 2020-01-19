496c784d1ea4536e0cbdcb71effae01ce3547431
daikideal <54879050+daikideal@users.noreply.github.com>
2020-01-19 17:08:41 +0900

    Add: Room.idを受け取り、監視する場所を分ける設定を追加
    'room_channel.js'の関数がページのレンダリング後に実行されるようにした。
    'room_channel.rb'のspeakアクションは、サンプルでは'user'のみのコードだったので、条件分岐でJobOffererかJobSeekerを判別するようにしてある。

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,6 +1,6 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "room_channel"
+    stream_from "room_channel_#{params['room']}"
   end
 
   def unsubscribed
@@ -9,6 +9,10 @@ class RoomChannel < ApplicationCable::Channel
 
   def speak(data)
     # ActionCable.server.broadcast 'room_channel', message: data['message']
-    Message.create! content: data['message']
+    if job_offerer_signed_in?
+      Message.create! content: data['message'], job_offerer_id: current_job_offerer.id, room_id: ['room']
+    else
+      Message.create! content: data['message'], job_seeker_id: current_job_seeker.id, room_id: ['room']
+    end
   end
 end
