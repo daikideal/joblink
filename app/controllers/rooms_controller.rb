db28457b40888503465d174b539ea8abc96aaf8f
daikideal <54879050+daikideal@users.noreply.github.com>
2020-01-19 16:17:34 +0900

    Add: Room一覧表示機能を追加
    Roomを全て最終更新の新しい順に表示するようにした。
    ログインしているユーザーの関係するルームのみを表示するようにしたり、パーシャル化して各々のマイページから飛べるようにしたりといろいろ改良の余地あり。

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,10 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
-  
+
+  def index
+    @rooms = Room.all.order(updated_at: :desc)
+  end
+
   def show
     @room = Room.find(params[:id])
     @messages = @room.messages
--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :rooms, only: %i[show]
+  resources :rooms, only: %i[index show]
 
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
