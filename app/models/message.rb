3fe2df8718e5039059676ebb78788abc4a5362dc
daikideal <54879050+daikideal@users.noreply.github.com>
2020-01-20 11:39:12 +0900

    Fix: バリデーションによりメッセージを送信できない不具合を修正
    'belongs_to'はデフォルトで全ての参照モデルが存在することを要求するため、複数のモデルがある場合、そのままでは送信できなくなるケースがある。
    例えば、job_offererがメッセージを送信するとき、job_offerer_idがあるのは当然と言えるが、デフォルトだと'Job Seekerを入力してください'というバリデーションエラーが出る。
    そのため、JobOffererとJobSeekerへの'belongs_to'には'optional: true'を付与し、どちらかが存在すれば良いという設定を実現した。
    条件つきアソシエーションなどがあればもう少しよい記法があるかもしれない。

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,8 @@
 class Message < ApplicationRecord
   validates :content, presence: true
 
-  belongs_to :job_offerer
-  belongs_to :job_seeker
+  belongs_to :job_offerer, optional: true
+  belongs_to :job_seeker, optional: true
   belongs_to :room
 
   after_create_commit { MessageBroadcastJob.perform_later self }
