703921725457b5faaaa44a028ddf684ca9e385df
daikideal <54879050+daikideal@users.noreply.github.com>
2020-01-25 23:07:18 +0900

    Add: resumeテーブルを作成
    resumeの項目を編集し、migrateした。

--- db/migrate/20200125134542_create_resumes.rb
+++ db/migrate/20200125134542_create_resumes.rb
@@ -1,6 +1,8 @@
 class CreateResumes < ActiveRecord::Migration[6.0]
   def change
     create_table :resumes do |t|
+      t.references :job_seeker, null: false, foreign_key: { on_delete: :cascade }
+      t.text :content
 
       t.timestamps
     end
