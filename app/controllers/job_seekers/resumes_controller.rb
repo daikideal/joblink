50a63213094b4a16033ee312e0c6271dbafed026
daikideal <54879050+daikideal@users.noreply.github.com>
2020-01-26 14:19:04 +0900

    Add: Resumeの新規作成・編集・削除機能を追加
    表示はJobSeekerのプロフィールで行えれば良いのでこの機能数にした。

--- app/controllers/job_seekers/resumes_controller.rb
+++ app/controllers/job_seekers/resumes_controller.rb
@@ -1,2 +1,55 @@
 class JobSeekers::ResumesController < ApplicationController
+  before_action :authenticate_job_seeker!
+  before_action :correct_job_seeker
+
+  def new
+    @resume = current_job_seeker.build_resume
+  end
+
+  def create
+    @resume = current_job_seeker.build_resume(resume_params)
+    if @resume.save
+      redirect_to job_seeker_path(current_job_seeker), notice: '経歴書を作成しました'
+    else
+      render new, alert: '経歴書の作成に失敗しました'
+    end
+  end
+
+  def edit
+    @resume = current_job_seeker.resume
+  end
+
+  def update
+    @resume = current_job_seeker.resume
+    if @resume.update_attributes(resume_params)
+      redirect_to job_seeker_path(current_job_seeker), notice: '経歴書を更新しました'
+    else
+      render 'edit', alert: '経歴書の更新に失敗しました'
+    end
+  end
+
+  def destroy
+    @job_seeker = JobSeeker.find(params[:job_seeker_id])
+    if @job_seeker.resume.destroy
+      redirect_to job_seeker_path(@job_seeker), notice: '経歴書を削除しました。'
+    else
+      redirect_to job_seeker_path(@job_seeker), alert: '経歴書の削除に失敗しました。'
+    end
+  end
+
+  private
+
+  def resume_params
+    params.require(:resume).permit(
+      :content
+    )
+  end
+
+  def correct_job_seeker
+    @job_seeker = JobSeeker.find(params[:job_seeker_id])
+    return if @job_seeker == current_job_seeker
+
+    redirect_back(fallback_location: root_path)
+    flash[:alert] = '権限がありません'
+  end
 end
