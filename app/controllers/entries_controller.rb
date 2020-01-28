e25eccd065688a7ceccb008f8f0783ccd328027b
daikideal <54879050+daikideal@users.noreply.github.com>
2020-01-28 17:59:05 +0900

    Fix: トーク相手が既に退出しているときに自分が退出できない不具合を修正
    current_job_offerer/current_job_seekerと既に退出して存在しない相手(@entry.job_seeker/@entry.job_offerer)を比較していたために起こった。
    条件式を自分のログイン状態だけにすることで修正。

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,9 +1,9 @@
 class EntriesController < ApplicationController
   def destroy
     @entry = current_user.entries.find_by(room_id: params[:id])
-    if current_job_offerer == @entry.job_offerer
+    if job_offerer_signed_in?
       @entry.update(job_offerer_id: nil)
-    elsif current_job_seeker == @entry.job_seeker
+    elsif job_seeker_signed_in?
       @entry.update(job_seeker_id: nil)
     end
     redirect_to rooms_path, notice: 'ルームを退出しました'
