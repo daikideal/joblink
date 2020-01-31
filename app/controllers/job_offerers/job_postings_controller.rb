329d882affaa37adc2cc5c45a047d881b17b754f
daikideal <54879050+daikideal@users.noreply.github.com>
2020-01-31 15:35:50 +0900

    Add: 作成主以外の編集・削除を受け付けないように設定を追加
    作成主かを判断するメソッドを定義し、before_actionに設定した。

--- app/controllers/job_offerers/job_postings_controller.rb
+++ app/controllers/job_offerers/job_postings_controller.rb
@@ -1,5 +1,6 @@
 class JobOfferers::JobPostingsController < ApplicationController
   before_action :authenticate_job_offerer!, except: %i[index show]
+  before_action :posting_require_correct_user, only: %i[edit update destroy]
 
   def index
     @job_postings = JobPosting.order(updated_at: :desc).page(params[:page]).per(5)
@@ -50,5 +51,12 @@ class JobOfferers::JobPostingsController < ApplicationController
       :title,
       :content
     )
+  end
+
+  def posting_require_correct_user
+    @job_posting = JobPosting.find(params[:id])
+    return unless @job_posting.job_offerer != current_job_offerer
+
+    redirect_back(fallback_location: root_path, alert: '権限がありません')
   end
 end
