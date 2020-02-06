cb8efc343c45a1595e8f5c0a82f64ae911d10cc3
daikideal <54879050+daikideal@users.noreply.github.com>
2020-02-06 11:01:22 +0900

    Add: 現在ログイン中のユーザーを返すメソッドを追加
    'application_controller'に同名のメソッドを定義していたが、viewの一部で動作しないことがあるので'application_helper'にもヘルパーメソッドとして定義しておいた。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+  def current_user
+    if job_offerer_signed_in?
+      current_job_offerer
+    elsif job_seeker_signed_in?
+      current_job_seeker
+    end
+  end
+
   def correct_user?
     if job_offerer_signed_in?
       return true if @job_offerer == current_job_offerer
