2b3dbfb747519e0827beec0d6e0f84ae82f8ffe7
daikideal <54879050+daikideal@users.noreply.github.com>
2020-02-07 09:51:33 +0900

    Add: マイグレーションファイルに外部キーを追加
    bookmarkテーブルにJobSeekerとJobPostingのidを参照するカラムを追加するように編集。

--- db/migrate/20200207001353_create_bookmarks.rb
+++ db/migrate/20200207001353_create_bookmarks.rb
@@ -1,8 +1,13 @@
 class CreateBookmarks < ActiveRecord::Migration[6.0]
   def change
     create_table :bookmarks do |t|
+      t.references :job_seeker, null: false
+      t.references :job_posting, null: false
 
       t.timestamps
     end
+
+    add_index :bookmarks, :job_seeker_id
+    add_index :bookmarks, :job_posting_id
   end
 end
