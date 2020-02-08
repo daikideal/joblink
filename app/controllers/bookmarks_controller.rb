9229b03be90db32d29ba42072b77d2aab4c15bf2
daikideal <54879050+daikideal@users.noreply.github.com>
2020-02-08 15:48:49 +0900

    Add: JobPostingをブックマークする機能を追加
    JobSeekerはJobPostingの詳細から、そのJobPostingをブックマークすることができる。
    お気に入りボタンはAjaxで非同期に'ブックマークしていない状態'と'ブックマーク状態'を切り替えられる。

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,20 @@
 class BookmarksController < ApplicationController
+  before_action :authenticate_job_seeker!
+
+  def create
+    set_posting
+    @bookmark = current_job_seeker.bookmarks.create(job_posting_id: params[:job_posting_id])
+  end
+
+  def destroy
+    set_posting
+    @bookmark = Bookmark.find_by(job_posting_id: params[:job_posting_id], job_seeker_id: current_job_seeker.id)
+    @bookmark.destroy
+  end
+
+  private
+
+  def set_posting
+    @job_posting = JobPosting.find(params[:job_posting_id])
+  end
 end
