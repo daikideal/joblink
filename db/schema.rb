fca40b2bf2ade6416645ea3632163569964d1edc
daikideal <54879050+daikideal@users.noreply.github.com>
2020-02-09 22:36:33 +0900

    Add: JobSeekerのテーブルに管理者状態のカラムを追加
    JobSeekerの側にも管理ユーザーを追加する可能性はあるので、カラムを追加しておいた。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_09_123055) do
+ActiveRecord::Schema.define(version: 2020_02_09_133514) do
 
   create_table "action_text_rich_texts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -143,6 +143,7 @@ ActiveRecord::Schema.define(version: 2020_02_09_123055) do
     t.datetime "locked_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
     t.index ["confirmation_token"], name: "index_job_seekers_on_confirmation_token", unique: true
     t.index ["email"], name: "index_job_seekers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_job_seekers_on_reset_password_token", unique: true
