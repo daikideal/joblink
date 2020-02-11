96610afd543bd13095d85713755b7239aa4e1a2d
daikideal <54879050+daikideal@users.noreply.github.com>
2020-02-11 15:36:23 +0900

    Change: seedのファイルを分割
    全て１つのseedファイルにまとめると見辛くなるので、'seeds'ディレクトリを新しく作成し、ユーザー種別ごとに管理するようにした。
    db直下の'seeds.rb'はそれらのファイルを'rails db:seed'で一括実行する。

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,5 @@
 # frozen_string_literal: true
 
-admin = JobOfferer.create!(
-  email: ENV['ADMIN_EMAIL'], password: ENV['ADMIN_PASSWORD'],
-  confirmed_at: Time.now, admin: true
-)
-admin.create_profile!(
-  first_name: ENV['ADMIN_FIRST_NAME'], last_name: ENV['ADMIN_LAST_NAME'],
-  first_name_furigana: ENV['ADMIN_FIRST_NAME_FURIGANA'],
-  last_name_furigana: ENV['ADMIN_LAST_NAME_FURIGANA'],
-  bio: 'ご利用ありがとうございます。', tag_list: ['web系']
-)
-admin.profile.avatar.attach(io: File.open('db/sample/avatar/admin.jpeg'), filename: 'admin.jpeg')
-
-test_offerer = JobOfferer.create!(
-  email: 'test_offerer@joblink.com', password: 'password',
-  confirmed_at: Time.now
-)
-test_offerer.create_profile!(
-  first_name: '求人者', last_name: '名無しの',
-  first_name_furigana: 'きゅうじんしゃ', last_name_furigana: 'ななしの',
-  bio: '簡単ログイン中のユーザーです'
-)
-
-test_seeker = JobSeeker.create!(
-  email: 'test_seeker@joblink.com', password: 'password',
-  confirmed_at: Time.now
-)
-test_seeker.create_profile!(
-  first_name: '求職者', last_name: '名無しの',
-  first_name_furigana: 'きゅうしょくしゃ', last_name_furigana: 'ななしの',
-  bio: '簡単ログイン中のユーザーです'
-)
+Dir.glob(File.join(Rails.root, 'db', 'seeds', '*.rb')) do |file|
+  load(file)
+end
