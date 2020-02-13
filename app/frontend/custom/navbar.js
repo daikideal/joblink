311929371cbfdc8a9092af800bc1a71c5685925e
daikideal <54879050+daikideal@users.noreply.github.com>
2020-02-13 15:05:48 +0900

    Add: materialize-sidenavのスクリプトを追加
    タブレット以下の画面サイズの時にクリックすると展開するサイドメニューの動作を追加した。
    ヘッダーのメニューは右に配置されているので右からスライドしてくる動作が妥当だと考え"edge: 'right'"のoptionを追加している。

--- app/frontend/custom/navbar.js
+++ app/frontend/custom/navbar.js
@@ -2,3 +2,12 @@
 $( document ).ready(function(){
   $(".dropdown-trigger").dropdown();
 });
+
+//sidenavのスライド処理
+$(document).ready(function(){
+  $('.sidenav').sidenav(
+    {
+      edge: 'right'
+    }
+  );
+});
