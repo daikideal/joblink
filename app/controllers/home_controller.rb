0c26da94307c22cb85ed307c857e668f33b95282
daikideal <54879050+daikideal@users.noreply.github.com>
2020-02-18 23:33:34 +0900

    Add: ログイン中のhomeページに最もアクティブなトークルームの表示を追加
    homeページからトレンドの求人とアクティブなトークルームに遷移できるようにすれば便利だろうと考えたので、追加した。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def home
+    @rooms = current_user.rooms.active.limit(3) if current_user.present?
     @popular_postings = JobPosting.popular
     @highlights = JobPosting.order(created_at: :desc).limit(5)
   end
