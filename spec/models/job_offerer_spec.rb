59a3236826314f390fae01dccf1b6acee2e66a40
daikideal <54879050+daikideal@users.noreply.github.com>
2020-02-25 08:55:45 +0900

    Clean: letで'job_offerer'/'job_seeker'を定義するよう変更
    何度もbuildを書かずに、カラムに代入さえすれば良いというようなコードにした。

--- spec/models/job_offerer_spec.rb
+++ spec/models/job_offerer_spec.rb
@@ -2,43 +2,45 @@ require 'rails_helper'
 
 RSpec.describe JobOfferer, type: :model do
   describe 'バリデーション' do
+    let(:job_offerer) { build(:job_offerer) }
+
     it 'メールアドレスとパスワードがあれば有効であること' do
-      expect(create(:job_offerer)).to be_valid
+      expect(job_offerer).to be_valid
     end
 
     it 'メールアドレスがなければ無効であること' do
-      expect(build(:job_offerer, email: '')).to_not be_valid
+      job_offerer.email = ''
+      expect(job_offerer).to_not be_valid
     end
 
     it 'メールアドレスが重複していると無効であること' do
-      job_offerer = create(:job_offerer, email: 'test@joblink.com')
-      expect(
-        build(:job_offerer, email: job_offerer.email)
-      ).to_not be_valid
+      job_offerer.save
+      expect(build(:job_offerer, email: job_offerer.email)).to_not be_valid
     end
 
     it 'メールアドレスが認証されていなければ無効であること' do
-      expect(
-        build(
-          :job_offerer, email: '', unconfirmed_email: 'test@joblink.com'
-        )
-      ).to_not be_valid
+      job_offerer.email = ''
+      job_offerer.unconfirmed_email = 'test@joblink.com'
+      expect(job_offerer).to_not be_valid
     end
 
     it 'パスワードがなければ無効であること' do
-      expect(build(:job_offerer, password: '')).to_not be_valid
+      job_offerer.password = ''
+      expect(job_offerer).to_not be_valid
     end
 
     it 'パスワードが6文字以上でなければ無効であること' do
-      expect(build(:job_offerer, password: 'a' * 5)).to_not be_valid
+      job_offerer.password = 'a' * 5
+      expect(job_offerer).to_not be_valid
     end
 
     it 'パスワードが128文字以内でなければ無効であること' do
-      expect(build(:job_offerer, password: 'a' * 129)).to_not be_valid
+      job_offerer.password = 'a' * 129
+      expect(job_offerer).to_not be_valid
     end
 
     it 'パスワードが暗号化されていること' do
-      job_offerer = create(:job_offerer)
+      job_offerer.save
       expect(job_offerer.encrypted_password).to_not eq 'password'
     end
   end
