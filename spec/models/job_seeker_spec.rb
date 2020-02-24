ff0caf22c6aa8a217c423f8b5b4e879c4c987caa
daikideal <54879050+daikideal@users.noreply.github.com>
2020-02-25 01:06:48 +0900

    Change: テストデータ生成の'FactoryBot'記述を削除
    rails_helperに'config.include FactoryBot::Syntax::Methods'を設定しているので、クラス名をいちいち書く必要はなかったらしい。

--- spec/models/job_seeker_spec.rb
+++ spec/models/job_seeker_spec.rb
@@ -2,42 +2,42 @@ require 'rails_helper'
 
 RSpec.describe JobSeeker, type: :model do
   it 'メールアドレスとパスワードがあれば有効であること' do
-    expect(FactoryBot.create(:job_seeker)).to be_valid
+    expect(create(:job_seeker)).to be_valid
   end
 
   it 'メールアドレスがなければ無効であること' do
-    expect(FactoryBot.build(:job_seeker, email: '')).to_not be_valid
+    expect(build(:job_seeker, email: '')).to_not be_valid
   end
 
   it 'メールアドレスが重複していると無効であること' do
-    job_seeker = FactoryBot.create(:job_seeker, email: 'test@joblink.com')
+    job_seeker = create(:job_seeker, email: 'test@joblink.com')
     expect(
-      FactoryBot.build(:job_seeker, email: job_seeker.email)
+      build(:job_seeker, email: job_seeker.email)
     ).to_not be_valid
   end
 
   it 'メールアドレスが認証されていなければ無効であること' do
     expect(
-      FactoryBot.build(
+      build(
         :job_seeker, email: '', unconfirmed_email: 'test@joblink.com'
       )
     ).to_not be_valid
   end
 
   it 'パスワードがなければ無効であること' do
-    expect(FactoryBot.build(:job_seeker, password: '')).to_not be_valid
+    expect(build(:job_seeker, password: '')).to_not be_valid
   end
 
   it 'パスワードが6文字以上でなければ無効であること' do
-    expect(FactoryBot.build(:job_seeker, password: 'a' * 5)).to_not be_valid
+    expect(build(:job_seeker, password: 'a' * 5)).to_not be_valid
   end
 
   it 'パスワードが128文字以内でなければ無効であること' do
-    expect(FactoryBot.build(:job_seeker, password: 'a' * 129)).to_not be_valid
+    expect(build(:job_seeker, password: 'a' * 129)).to_not be_valid
   end
 
   it 'パスワードが暗号化されていること' do
-    job_seeker = FactoryBot.create(:job_seeker)
+    job_seeker = create(:job_seeker)
     expect(job_seeker.encrypted_password).to_not eq 'password'
   end
 end
