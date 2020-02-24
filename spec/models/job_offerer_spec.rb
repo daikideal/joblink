44dbd8e685132e66e08b6ea2fb42e40beac04fea
daikideal <54879050+daikideal@users.noreply.github.com>
2020-02-25 03:04:30 +0900

    Add: ネストしたdescribeを追加
    アカウントの有効性のテストであることがわかりやすいようにした。

--- spec/models/job_offerer_spec.rb
+++ spec/models/job_offerer_spec.rb
@@ -1,43 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe JobOfferer, type: :model do
-  it 'メールアドレスとパスワードがあれば有効であること' do
-    expect(create(:job_offerer)).to be_valid
-  end
+  describe 'バリデーション' do
+    it 'メールアドレスとパスワードがあれば有効であること' do
+      expect(create(:job_offerer)).to be_valid
+    end
 
-  it 'メールアドレスがなければ無効であること' do
-    expect(build(:job_offerer, email: '')).to_not be_valid
-  end
+    it 'メールアドレスがなければ無効であること' do
+      expect(build(:job_offerer, email: '')).to_not be_valid
+    end
 
-  it 'メールアドレスが重複していると無効であること' do
-    job_offerer = create(:job_offerer, email: 'test@joblink.com')
-    expect(
-      build(:job_offerer, email: job_offerer.email)
-    ).to_not be_valid
-  end
+    it 'メールアドレスが重複していると無効であること' do
+      job_offerer = create(:job_offerer, email: 'test@joblink.com')
+      expect(
+        build(:job_offerer, email: job_offerer.email)
+      ).to_not be_valid
+    end
 
-  it 'メールアドレスが認証されていなければ無効であること' do
-    expect(
-      build(
-        :job_offerer, email: '', unconfirmed_email: 'test@joblink.com'
-      )
-    ).to_not be_valid
-  end
+    it 'メールアドレスが認証されていなければ無効であること' do
+      expect(
+        build(
+          :job_offerer, email: '', unconfirmed_email: 'test@joblink.com'
+        )
+      ).to_not be_valid
+    end
 
-  it 'パスワードがなければ無効であること' do
-    expect(build(:job_offerer, password: '')).to_not be_valid
-  end
+    it 'パスワードがなければ無効であること' do
+      expect(build(:job_offerer, password: '')).to_not be_valid
+    end
 
-  it 'パスワードが6文字以上でなければ無効であること' do
-    expect(build(:job_offerer, password: 'a' * 5)).to_not be_valid
-  end
+    it 'パスワードが6文字以上でなければ無効であること' do
+      expect(build(:job_offerer, password: 'a' * 5)).to_not be_valid
+    end
 
-  it 'パスワードが128文字以内でなければ無効であること' do
-    expect(build(:job_offerer, password: 'a' * 129)).to_not be_valid
-  end
+    it 'パスワードが128文字以内でなければ無効であること' do
+      expect(build(:job_offerer, password: 'a' * 129)).to_not be_valid
+    end
 
-  it 'パスワードが暗号化されていること' do
-    job_offerer = create(:job_offerer)
-    expect(job_offerer.encrypted_password).to_not eq 'password'
+    it 'パスワードが暗号化されていること' do
+      job_offerer = create(:job_offerer)
+      expect(job_offerer.encrypted_password).to_not eq 'password'
+    end
   end
 end
--- spec/models/job_seeker_spec.rb
+++ spec/models/job_seeker_spec.rb
@@ -1,43 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe JobSeeker, type: :model do
-  it 'メールアドレスとパスワードがあれば有効であること' do
-    expect(create(:job_seeker)).to be_valid
-  end
+  describe 'バリデーション' do
+    it 'メールアドレスとパスワードがあれば有効であること' do
+      expect(create(:job_seeker)).to be_valid
+    end
 
-  it 'メールアドレスがなければ無効であること' do
-    expect(build(:job_seeker, email: '')).to_not be_valid
-  end
+    it 'メールアドレスがなければ無効であること' do
+      expect(build(:job_seeker, email: '')).to_not be_valid
+    end
 
-  it 'メールアドレスが重複していると無効であること' do
-    job_seeker = create(:job_seeker, email: 'test@joblink.com')
-    expect(
-      build(:job_seeker, email: job_seeker.email)
-    ).to_not be_valid
-  end
+    it 'メールアドレスが重複していると無効であること' do
+      job_seeker = create(:job_seeker, email: 'test@joblink.com')
+      expect(
+        build(:job_seeker, email: job_seeker.email)
+      ).to_not be_valid
+    end
 
-  it 'メールアドレスが認証されていなければ無効であること' do
-    expect(
-      build(
-        :job_seeker, email: '', unconfirmed_email: 'test@joblink.com'
-      )
-    ).to_not be_valid
-  end
+    it 'メールアドレスが認証されていなければ無効であること' do
+      expect(
+        build(
+          :job_seeker, email: '', unconfirmed_email: 'test@joblink.com'
+        )
+      ).to_not be_valid
+    end
 
-  it 'パスワードがなければ無効であること' do
-    expect(build(:job_seeker, password: '')).to_not be_valid
-  end
+    it 'パスワードがなければ無効であること' do
+      expect(build(:job_seeker, password: '')).to_not be_valid
+    end
 
-  it 'パスワードが6文字以上でなければ無効であること' do
-    expect(build(:job_seeker, password: 'a' * 5)).to_not be_valid
-  end
+    it 'パスワードが6文字以上でなければ無効であること' do
+      expect(build(:job_seeker, password: 'a' * 5)).to_not be_valid
+    end
 
-  it 'パスワードが128文字以内でなければ無効であること' do
-    expect(build(:job_seeker, password: 'a' * 129)).to_not be_valid
-  end
+    it 'パスワードが128文字以内でなければ無効であること' do
+      expect(build(:job_seeker, password: 'a' * 129)).to_not be_valid
+    end
 
-  it 'パスワードが暗号化されていること' do
-    job_seeker = create(:job_seeker)
-    expect(job_seeker.encrypted_password).to_not eq 'password'
+    it 'パスワードが暗号化されていること' do
+      job_seeker = create(:job_seeker)
+      expect(job_seeker.encrypted_password).to_not eq 'password'
+    end
   end
 end
