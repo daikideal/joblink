c66d9b22a4ecf1929b5f42470a68c8546ed0bf84
daikideal <54879050+daikideal@users.noreply.github.com>
2020-02-28 02:38:36 +0900

    Change: バリデーションテストのデータををbuildで生成するように変更
    '作成する時'のバリデーションなのでbuildにするのが相当であると考えた。

--- spec/models/job_seeker_profile_spec.rb
+++ spec/models/job_seeker_profile_spec.rb
@@ -1,37 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe JobSeekerProfile, type: :model do
-  let(:profile) { create(:job_seeker_profile) }
-
   describe 'バリデーション' do
     it '姓, 名, 姓(ふりがな), 名(ふりがな)があれば有効であること' do
-      expect(profile).to be_valid
+      expect(build(:job_seeker_profile)).to be_valid
     end
 
     it '姓がなければ無効であること' do
-      profile.last_name = ''
-      expect(profile).to_not be_valid
+      expect(build(:job_seeker_profile, last_name: '')).to_not be_valid
     end
 
     it '名がなければ無効であること' do
-      profile.first_name = ''
-      expect(profile).to_not be_valid
+      expect(build(:job_seeker_profile, first_name: '')).to_not be_valid
     end
 
     it '姓(ふりがな)がなければ無効であること' do
-      profile.last_name_furigana = ''
-      expect(profile).to_not be_valid
+      expect(
+        build(:job_seeker_profile, last_name_furigana: '')
+      ).to_not be_valid
     end
 
     it '名(ふりがな)がなければ無効であること' do
-      profile.first_name_furigana = ''
-      expect(profile).to_not be_valid
+      expect(
+        build(:job_seeker_profile, first_name_furigana: '')
+      ).to_not be_valid
     end
 
     it 'bioは160文字以内であること' do
-      profile.bio = 'a' * 161
-      expect(profile).to_not be_valid
+      expect(build(:job_seeker_profile, bio: 'a' * 161)).to_not be_valid
     end
+  end
+
+  describe 'アバター' do
+    let(:profile) { create(:job_seeker_profile) }
 
     it 'アバター画像を追加できること' do
       profile.avatar.attach(
@@ -42,6 +43,8 @@ RSpec.describe JobSeekerProfile, type: :model do
   end
 
   describe 'フルネーム' do
+    let(:profile) { create(:job_seeker_profile) }
+
     it '姓と名をつなげたフルネームを返すこと' do
       full_name = profile.last_name + ' ' + profile.first_name
       expect(profile.full_name).to eq(full_name)
