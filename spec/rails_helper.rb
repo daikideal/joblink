54a493d4bfc688a6feece6286a4bae06b4827de6
daikideal <54879050+daikideal@users.noreply.github.com>
2020-02-29 15:07:40 +0900

    Add: type: :requestのテストでdeviseのヘルパーメソッドが使えるよう設定を追加
    ログイン状態でテストしたい項目があったので、deviseのsign_inメソッド等を使用できるようにした。
    Rspecのヘルパーメソッドを配置するディレクトリは'support'という名前が一般的だったので変更した。

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,5 +61,9 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  config.include Devise::TestHelpers, type: :controller
+
+  config.include RequestSpecHelper, type: :request
+
   config.include FactoryBot::Syntax::Methods
 end
