ff3c4fc1d44f984cba683d17708c5adc07abaaec
daikideal <54879050+daikideal@users.noreply.github.com>
2020-02-29 11:30:56 +0900

    Add: 有効性のテストを追加
    定義したアソシエーションの通りにトークルーム閲覧・入室の権限が存在できることを検証するテストを追加した。

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Entry, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:entry) { create(:entry) }
+
+  describe 'バリデーション' do
+    it '有効な入室権限が作成できること' do
+      expect(build(:entry)).to be_valid
+    end
+
+    it '求人者が退出しても求職者の入室権限が有効であること' do
+      entry.job_offerer = nil
+      expect(entry).to be_valid
+    end
+
+    it '求職者が退出しても求人者の入室権限が有効であること' do
+      entry.job_seeker = nil
+      expect(entry).to be_valid
+    end
+  end
 end
