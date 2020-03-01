1aa76c4f25955bbb6547b24c615d89e84bd817f8
daikideal <54879050+daikideal@users.noreply.github.com>
2020-03-01 13:48:00 +0900

    Add: 求人のリクエストテストを追加
    JobPostingに関するリクエストとレスポンスのテストを追加した。

--- spec/factories/job_postings.rb
+++ spec/factories/job_postings.rb
@@ -3,5 +3,14 @@ FactoryBot.define do
     association :job_offerer
     title { 'a' * 40 }
     content { '<h1>テスト</h1><div>テストデータ</div>' }
+
+    trait :invalid do
+      title { nil }
+    end
+
+    trait :another do
+      title { 'b' * 40 }
+      content { '<h1>テスト2</h1><div>テストデータ2</div>' }
+    end
   end
 end
