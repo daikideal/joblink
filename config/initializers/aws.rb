5c0fdadf179e8eab4fb2f617554f71bec43c1b32
daikideal <54879050+daikideal@users.noreply.github.com>
2020-03-24 02:35:57 +0900

    Add: deviseのメールにAWS SESの設定を追加
    本番環境のメールをAWSのSESを利用して送信するようにした。

---
+++ config/initializers/aws.rb
@@ -0,0 +1,7 @@
+ActionMailer::Base.add_delivery_method(
+  :ses,
+  AWS::SES::Base,
+  access_key_id: Rails.application.credentials.ses[:access_key_id],
+  secret_access_key: Rails.application.credentials.ses[:secret_access_key],
+  server: 'email.us-east-1.amazonaws.com'
+)
